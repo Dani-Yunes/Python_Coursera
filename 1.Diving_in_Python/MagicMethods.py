0242303be541089ee2ec900295fd6127fa255364
Dani_Yunes <danilothebest223@yandex.ru>
2021-03-10 23:06:49 +0300

    Finish MagicMethod

--- 1.Diving_in_Python/MagicMethods.py
+++ 1.Diving_in_Python/MagicMethods.py
@@ -1,3 +1,41 @@
-from tempfile import gettempdir
-from os.path import join
+import tempfile
+import os
+
+class File:
+
+    def __init__(self, full_filepath): #Инициализация
+        self.full_filepath = full_filepath
+        self.seek = 0
+
+    def __str__(self): #Вывод
+        return self.full_filepath
+
+    def __iter__(self):
+        return self
+
+    def __next__(self): #Итерация по строкам
+        with open(self.full_filepath) as f:
+            f.seek(self.seek)
+            result = f.readline()
+            self.seek = f.tell()
+            if result == '':
+                raise StopIteration
+            return result
+
+    def __add__(self, second_file): #Сложение двух File-ов
+        new_path = os.path.join(tempfile.gettempdir(), 'new_file.txt')
+        with open(new_path, 'w') as f:
+            for line in self:
+                f.write(line)
+            for line in second_file:
+                f.write(line)
+            f.write('\n')
+        return File(new_path)
+
+
+    def write(self, text):
+        with open(self.full_filepath, 'w') as f:
+            f.write(text)
+
+
 
