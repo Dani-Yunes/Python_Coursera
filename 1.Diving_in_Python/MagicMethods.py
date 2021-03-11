5145954d8ceff025a55d33d50c65bd8a7d4c1984
Dani_Yunes <danilothebest223@yandex.ru>
2021-03-11 11:48:31 +0300

    MagicMethod.py is finished

--- 1.Diving_in_Python/MagicMethods.py
+++ 1.Diving_in_Python/MagicMethods.py
@@ -1,9 +1,15 @@
 import tempfile
 import os
+import uuid
 
 class File:
 
     def __init__(self, full_filepath): #Инициализация
+        if os.path.exists(full_filepath):
+            pass
+        else:
+            with open(full_filepath, 'w') as f:
+                pass
         self.full_filepath = full_filepath
         self.seek = 0
 
@@ -19,17 +25,22 @@ class File:
             result = f.readline()
             self.seek = f.tell()
             if result == '':
+                self.seek = 0
                 raise StopIteration
             return result
 
+    def read(self): #Чтение из файла
+        with open(self.full_filepath, 'r') as f:
+            return f.read()
+
+
     def __add__(self, second_file): #Сложение двух File-ов
-        new_path = os.path.join(tempfile.gettempdir(), 'new_file.txt')
+        new_path = os.path.join(tempfile.gettempdir(), str(uuid.uuid4()))
         with open(new_path, 'w') as f:
             for line in self:
                 f.write(line)
             for line in second_file:
                 f.write(line)
-            f.write('\n')
         return File(new_path)
 
 
